bde71cd45ea3993b22c391274480b63fd67bc61b
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-03 14:21:11 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -22,15 +22,17 @@ def output_array(array)
   
   while count < array.length do
     puts array[count]
+    count += 1
   end
 end
 
 def return_string_array(array)
   count = 0
+  new_array= []
   while count < array.length do
-    array[count].to_s
-    puts array
+    new_array.push(array[count].to_s)
     count +=1
   end
+  new_array
 end
   
